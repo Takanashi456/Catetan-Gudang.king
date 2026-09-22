8c94ec0fa9bd30313d1dc80b882d5c65ed0d3169
Ridki <ridkiridki743@gmail.com>
2026-09-22 22:33:16 +0700

    1
    1

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="stokku-v1";
+const FILES=["./","./index.html","./style.css","./script.js","./manifest.json","./icon.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
